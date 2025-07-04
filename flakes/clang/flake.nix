36b4d0e74f10736953977c7bf73878521aa37e65
zokrezyl <zokrezyl@gmail.com>
2025-07-04 13:06:46 +0200

    initial structure and flakes

--- flakes/clang/flake.nix
+++ flakes/clang/flake.nix
@@ -1,15 +1,24 @@
 {
   description = "Clang + libc++ + CMake dev shell";
 
-  #inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }: {
+    packages = {
+      x86_64-linux = let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in {
+        dev-shell-bin = pkgs.writeShellApplication {
+          name = "dev-shell";
+          runtimeInputs = [ pkgs.bash ];
+          text = builtins.readFile ./bin/dev-shell;
+        };
+      };
+    };
+
     devShells = {
       x86_64-linux = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
         llvm = pkgs.llvmPackages;
       in {
         default = llvm.libcxxStdenv.mkDerivation {
@@ -19,11 +28,11 @@
             llvm.lld
             pkgs.cmake
             pkgs.python314
+            self.packages.x86_64-linux.dev-shell-bin
           ];
           shellHook = ''
-            exec ./bin/dev-shell
+            exec dev-shell
           '';
-
         };
       };
     };
